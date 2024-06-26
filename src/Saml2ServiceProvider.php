9c64a0e42277d729b01c1830d7fded669021982e
Julius Kiekbusch <contact@julius-kiekbusch.de>
2024-06-26 17:42:23 +0200

    Register SAML2 Socialite Driver

--- src/Saml2ServiceProvider.php
+++ src/Saml2ServiceProvider.php
@@ -3,7 +3,10 @@
 namespace StuRaBtu\Saml2;
 
 use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+use SocialiteProviders\Saml2\Saml2ExtendSocialite;
 
 class Saml2ServiceProvider extends ServiceProvider
 {
@@ -12,5 +15,8 @@ class Saml2ServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/../routes/saml2.php');
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'saml2');
         $this->loadTranslationsFrom(__DIR__ . '/../lang/saml2.php', 'saml2');
+
+        /** Register SAML2 Socialite Provider */
+        Event::listen(SocialiteWasCalled::class, Saml2ExtendSocialite::class);
     }
 }
